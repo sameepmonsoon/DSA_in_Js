e92b1aa55e2cf2390263d3daf243dd42a32a5384
Sameep Rana <sameep.monsoon@gmail.com>
2023-06-04 22:45:55 +0545

    Dsa leetcode problems and solutions

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,13 @@
+// Given an integer x, return true if x is a
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
 var isPalindrome = function (x) {
   return x < 0 ? false : x === +x.toString().split("").reverse().join("");
 };
